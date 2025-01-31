6ef68122bde0a88b42a752c63913db42fc73d2ee
Slimobedsama <ohwo.okoro@yahoo.com>
2025-01-31 11:55:06 +0100

    added cors middleware

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 const moment = require('moment');
 moment().format();
 
@@ -8,14 +9,15 @@ const PORT = 3001;
 app.use(express.urlencoded({extended: false}))
 app.use(express.json());
 
-app.get('/info', (req, res) => {
-    const date = moment().toISOString();
+app.get('/info', cors(), (req, res) => {
+    const date = moment().toISOString().substring(0, 19) + 'Z';
     const studentData = {
         email: 'ohwo.okoro@yahoo.com',
         current_datetime: date,
+        github_url: 'https://github.com/Slimobedsama/hng_12_task_0'
     }
     res.json(studentData)
 });
 
-// Correctly specify the port in the listen method
+
 app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
